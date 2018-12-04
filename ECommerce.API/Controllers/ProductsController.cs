9f41aef213f8bc79f22a7bba50fb4110851b56c0
er.kkmohanty@gmail.com <er.kkmohanty@gmail.com>
2018-12-04 12:36:14 +0530

    service fabric

---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<ApiProduct>> GetAsync()
+        {
+            return new[] {new ApiProduct {Id = Guid.NewGuid(), Description = "fake"}};
+        }
+        [HttpPost]
+        public async Task Post([FromBody] ApiProduct product)
+        {
+
+
+        }
+
+        
+    }
+}
--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.ServiceFabric.Services.Runtime;
+using System;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.ServiceFabric.Services.Runtime;
 
-namespace ECommerce.ProductCatalog
+namespace ECommerce.API
 {
     internal static class Program
     {
@@ -20,12 +20,12 @@ namespace ECommerce.ProductCatalog
                 // When Service Fabric creates an instance of this service type,
                 // an instance of the class is created in this host process.
 
-                ServiceRuntime.RegisterServiceAsync("ECommerce.ProductCatalogType",
-                    context => new ProductCatalog(context)).GetAwaiter().GetResult();
+                ServiceRuntime.RegisterServiceAsync("ECommerce.APIType",
+                    context => new API(context)).GetAwaiter().GetResult();
 
-                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(ProductCatalog).Name);
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(API).Name);
 
-                // Prevents this host process from terminating so services keep running.
+                // Prevents this host process from terminating so services keeps running. 
                 Thread.Sleep(Timeout.Infinite);
             }
             catch (Exception e)
